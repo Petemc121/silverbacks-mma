878c9587c8206978a0ff49595cbc47c38b2da952
Petemc121 <petemc121@gmail.com>
2021-12-25 15:25:53 +0000

    added maps to location

--- src/pages/Location.tsx
+++ src/pages/Location.tsx
@@ -2,8 +2,36 @@ import React from "react";
 import Header from "../components/Header";
 import HeroImage from "../components/HeroImage";
 import gorilla from "../images/gorilla.png";
+import { GoogleMap, useJsApiLoader } from "@react-google-maps/api";
+
+const containerStyle = {
+  width: "400px",
+  height: "400px",
+};
+
+const center = {
+  lat: -3.745,
+  lng: -38.523,
+};
 
 export default function Location() {
+  const { isLoaded } = useJsApiLoader({
+    id: "google-map-script",
+    googleMapsApiKey: "AIzaSyD-1HmMYXD3JTqFgrQGi2qNABUgnWjsRks",
+  });
+
+  const [map, setMap] = React.useState(null);
+
+  const onLoad = React.useCallback(function callback(map) {
+    const bounds = new window.google.maps.LatLngBounds();
+    map.fitBounds(bounds);
+    setMap(map);
+  }, []);
+
+  const onUnmount = React.useCallback(function callback(map) {
+    setMap(null);
+  }, []);
+
   const imgProps = {
     id: 1,
     src: gorilla,
@@ -18,7 +46,7 @@ export default function Location() {
     imageContainerContent: "center",
   };
 
-  return (
+  return isLoaded ? (
     <div className="App">
       <div id="aboutHeaderSliderContainer">
         <Header />
@@ -26,6 +54,25 @@ export default function Location() {
           <HeroImage slidePosition={"1"} heroImage={imgProps} />
         </div>
       </div>
+    </div>
+  ) : (
+    <div className="App">
+      <div id="aboutHeaderSliderContainer">
+        <Header />
+        <div className="heroContainer" id="aboutHeroContainer">
+          <HeroImage slidePosition={"1"} heroImage={imgProps} />
+        </div>
+      </div>
+      <GoogleMap
+        mapContainerStyle={containerStyle}
+        center={center}
+        zoom={10}
+        onLoad={onLoad}
+        onUnmount={onUnmount}
+      >
+        {/* Child components, such as markers, info windows, etc. */}
+        <></>
+      </GoogleMap>
     </div>
   );
 }
