745810c4344cc3adb7947ad2b55b561f5b383ab9
Petemc121 <petemc121@gmail.com>
2021-12-27 17:23:47 +0000

    adapted for routing

--- src/pages/Location.tsx
+++ src/pages/Location.tsx
@@ -2,36 +2,9 @@ import React from "react";
 import Header from "../components/Header";
 import HeroImage from "../components/HeroImage";
 import gorilla from "../images/gorilla.png";
-import { GoogleMap, useJsApiLoader } from "@react-google-maps/api";
-
-const containerStyle = {
-  width: "400px",
-  height: "400px",
-};
-
-const center = {
-  lat: -3.745,
-  lng: -38.523,
-};
+import Map from "../components/Map";
 
 export default function Location() {
-  const { isLoaded } = useJsApiLoader({
-    id: "google-map-script",
-    googleMapsApiKey: "AIzaSyD-1HmMYXD3JTqFgrQGi2qNABUgnWjsRks",
-  });
-
-  const [map, setMap] = React.useState(null);
-
-  const onLoad = React.useCallback(function callback(map) {
-    const bounds = new window.google.maps.LatLngBounds();
-    map.fitBounds(bounds);
-    setMap(map);
-  }, []);
-
-  const onUnmount = React.useCallback(function callback(map) {
-    setMap(null);
-  }, []);
-
   const imgProps = {
     id: 1,
     src: gorilla,
@@ -46,7 +19,7 @@ export default function Location() {
     imageContainerContent: "center",
   };
 
-  return isLoaded ? (
+  return (
     <div className="App">
       <div id="aboutHeaderSliderContainer">
         <Header />
@@ -54,25 +27,7 @@ export default function Location() {
           <HeroImage slidePosition={"1"} heroImage={imgProps} />
         </div>
       </div>
-    </div>
-  ) : (
-    <div className="App">
-      <div id="aboutHeaderSliderContainer">
-        <Header />
-        <div className="heroContainer" id="aboutHeroContainer">
-          <HeroImage slidePosition={"1"} heroImage={imgProps} />
-        </div>
-      </div>
-      <GoogleMap
-        mapContainerStyle={containerStyle}
-        center={center}
-        zoom={10}
-        onLoad={onLoad}
-        onUnmount={onUnmount}
-      >
-        {/* Child components, such as markers, info windows, etc. */}
-        <></>
-      </GoogleMap>
+      <Map />
     </div>
   );
 }
